28d54046ee61b6726e1612c40025233bdbc653f4
kalu prajapati <kaluprajapati1907@gmail.com>
2025-12-18 19:06:24 +0530

    commit

---
+++ one.js
@@ -0,0 +1,8 @@
+let emailid = "kp@10gmail.com"
+const ac_number = 2234567
+ac_name = "saving"
+let ac_deleted;
+
+
+console.table([emailid,ac_number,ac_name.ac_deleted])
+/*  mostely use let because of issue of block space */
